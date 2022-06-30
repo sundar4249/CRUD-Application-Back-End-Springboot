aedd2840ba8a457f92f964004781e821b26693e9
sundar chaudhary <adoring.alis4249@gmail.comgit config --global user.email adoring.alis4249@gmail.com>
2022-06-30 23:27:50 +0545

    backend

---
+++ springboot-backend/src/main/java/chaudhary/sundar/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package chaudhary.sundar.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import chaudhary.sundar.springboot.model.Employee;
+
+@Repository   
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
